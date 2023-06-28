ccd998caa80302a3b928f921a819ca14fc8dca5a
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-06-28 18:46:49 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="flex justify-center h-[10vh] items-center bg-zinc-800 w-full p-6 shadow-md">
-      <strong className="text-zinc-300">SENASP/MJ</strong>
+      <strong className="text-zinc-300">SENASP/MJSP</strong>
     </header>
   )
 }
