bf63e35eba8fd8f4645fcc8ee2b191e6457454d4
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-06-29 13:40:14 -0300

    add favicon and logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
+
 export default function Header() {
   return (
     <header className="flex justify-center h-[10vh] items-center bg-zinc-800 w-full p-6 shadow-md">
-      <strong className="text-zinc-300">SENASP/MJSP</strong>
+      <img className="max-w-[75px]" src="logo-mjsp.png" alt="Logo do MJSP" />
     </header>
   )
 }
